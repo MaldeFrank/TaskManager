a9fb6664f0ee67b01b394a8bb118c7a79863b93e
MaldeFrank <maldefrankmidtgaard@gmail.com>
2024-11-06 20:18:12 +0100

    Added delete funtion for profile

--- src/main/java/com/example/cleanappbackend/controller/ProfileController.java
+++ src/main/java/com/example/cleanappbackend/controller/ProfileController.java
@@ -3,10 +3,7 @@ package com.example.cleanappbackend.controller;
 import com.example.cleanappbackend.model.Profile;
 import com.example.cleanappbackend.repository.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,14 @@ public class ProfileController {
     @PostMapping("/profile")
     Profile createProfile(@RequestBody Profile profile){
         return repository.save(profile);
+    }
+
+    @DeleteMapping("/profile/{id}")
+    void deleteProfile(@PathVariable Long id){
+        Profile profile = repository.getReferenceById(id);
+        profile.getAssignedTasks().forEach(assignedTask ->{
+            assignedTask.setAssignedTo(null);
+        } );
+        repository.deleteById(id);
     }
 }
