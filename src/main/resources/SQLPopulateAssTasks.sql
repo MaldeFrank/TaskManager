18c2e8e81effca279d6e492d466c02668301d205
MaldeFrank <maldefrankmidtgaard@gmail.com>
2025-02-16 14:10:08 +0100

    period filter

--- src/main/resources/SQLPopulateAssTasks.sql
+++ src/main/resources/SQLPopulateAssTasks.sql
@@ -1,8 +1,8 @@
 INSERT INTO assigned_task (completed, date_time, id, task_id, task_list_id, user_id)
-VALUES (false, '2025-02-10', 2, 1, 1, '118261767348339964642');
-
-INSERT INTO assigned_task (completed, date_time, id, task_id, task_list_id, user_id)
 VALUES (false, '2025-02-10', 3, 1, 1, '118261767348339964642');
 
 INSERT INTO assigned_task (completed, date_time, id, task_id, task_list_id, user_id)
-VALUES (false, '2025-03-10', 4, 1, 1, '118261767348339964642');
+VALUES (false, '2025-02-10', 4, 1, 1, '118261767348339964642');
+
+INSERT INTO assigned_task (completed, date_time, id, task_id, task_list_id, user_id)
+VALUES (false, '2025-03-10', 5, 1, 1, '118261767348339964642');
