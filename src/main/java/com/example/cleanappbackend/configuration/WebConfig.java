e0a923d2518185811928eef80f157924a0bcb47c
MaldeFrank <maldefrankmidtgaard@gmail.com>
2025-03-13 21:02:26 +0100

    webconfig

--- src/main/java/com/example/cleanappbackend/configuration/WebConfig.java
+++ src/main/java/com/example/cleanappbackend/configuration/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://172.22.80.1:3000")
+                .allowedOrigins("http://161.35.196.6:80")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
