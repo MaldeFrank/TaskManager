c1ee9bc8a04bdef614e7cc0cb9d95e16467ec23f
MaldeFrank <maldefrankmidtgaard@gmail.com>
2025-04-01 11:51:02 +0200

    TasklistDto

--- src/main/java/com/example/cleanappbackend/model/dto/TasklistDto.java
+++ src/main/java/com/example/cleanappbackend/model/dto/TasklistDto.java
@@ -1,16 +1,22 @@
 package com.example.cleanappbackend.model.dto;
 import com.example.cleanappbackend.model.Tasklist;
 import lombok.Data;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class TasklistDto {
     private long taskId;
     private String listName;
     private String periodFilter;
+    private List<AssignedTaskDto> assignedTaskList;
 
     public TasklistDto(Tasklist tasklist) {
         this.taskId = tasklist.getTaskId();
         this.listName = tasklist.getListName();
         this.periodFilter = tasklist.getPeriodFilter();
+        this.assignedTaskList = tasklist.getAssignedTaskList().stream()
+                .map(AssignedTaskDto::new)
+                .collect(Collectors.toList());
     }
 }
