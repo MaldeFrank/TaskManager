80e170ded84b95cb9f40cfece490858a5d6b2710
MaldeFrank <maldefrankmidtgaard@gmail.com>
2025-04-01 13:24:05 +0200

    CORS

--- src/main/java/com/example/cleanappbackend/configuration/WebConfig.java
+++ src/main/java/com/example/cleanappbackend/configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
-                    .allowedOrigins("www.pligtlisten.dk")
+                    .allowedOrigins("https://www.pligtlisten.dk")
                     .allowedMethods("GET", "POST", "PUT", "DELETE")
                     .allowedHeaders("*")
                     .allowCredentials(true);
